87adc21ee423001a152a51d458dc7de6a0061c7f
Leisan Kazberova <leisan.kh@gmail.com>
2016-02-20 00:45:38 +0300

    fix problem with video

--- index.js
+++ index.js
@@ -62,22 +62,67 @@
     };
 
     var applyFilter = function () {
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
-                var pixel = canvas.getContext('2d').getImageData(x, y, 1, 1);
+        var filters = {
+            invert: function (pixel) {
+                pixel[0] = 255 - pixel[0];
+                pixel[1] = 255 - pixel[1];
+                pixel[2] = 255 - pixel[2];
 
-                pixel.data = applyFilterToPixel(pixel.data);
+                return pixel;
+            },
+            grayscale: function (pixel) {
+                var r = pixel[0];
+                var g = pixel[1];
+                var b = pixel[2];
+                var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-                canvas.getContext('2d').putImageData(pixel, x, y);
+                pixel[0] = pixel[1] = pixel[2] = v;
+
+                return pixel;
+            },
+            threshold: function (pixel) {
+                var r = pixel[0];
+                var g = pixel[1];
+                var b = pixel[2];
+                var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= 128) ? 255 : 0;
+                pixel[0] = pixel[1] = pixel[2] = v;
+
+                return pixel;
             }
-        }
+        };
+
+        var context     = canvas.getContext('2d');
+        var videoData   = context.getImageData(0, 0, canvas.width, canvas.height);
+        var filterName  = document.querySelector('.controls__filter').value;
+
+        var filteredData = applyFilterToImageData (videoData,filters[filterName]);
+        context.putImageData(filteredData, 0, 0);
     };
+
+    var applyFilterToImageData = function (imageData, filter) {
+        var pixels = imageData.data;
+
+        // каждому пикселю изображения соответствует 4 значения - r,g,b и alpha
+        for (var i = 0; i < pixels.length; i += 4) {
+            var r = pixels[i];
+            var g = pixels[i + 1];
+            var b = pixels[i + 2];
+            var filteredPixel = filter([r,g,b]);
+
+            pixels[i]     = filteredPixel[0]; // r
+            pixels[i + 1] = filteredPixel[1]; // g
+            pixels[i + 2] = filteredPixel[2]; // b
+        }
+        return imageData;
+    };
+
 
     var captureFrame = function () {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
 
         canvas.getContext('2d').drawImage(video, 0, 0);
+
         applyFilter();
     };
 
